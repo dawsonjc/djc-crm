62f381de2a87d9c511c950bf1d20d108465eb583
Dawson Chatfield <thedeadgamer7777@gmail.com>
2026-09-11 22:07:35 -0700

    Stop tracking js file output

--- frontend/src/main/webapp/static/js/admin.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-import * as $j_com$002ebrewery$002eweb$002efrontend$002eentry$002e$002dAdmin$002dMain from "./com.brewery.web.frontend.entry.-Admin-Main.js";
-import * as $j_internal$002d07235935c771c9429268cb101d7f2e2e7f60988c from "./internal-07235935c771c9429268cb101d7f2e2e7f60988c.js";
-$j_com$002ebrewery$002eweb$002efrontend$002eentry$002e$002dAdmin$002dMain.$s_Lcom_brewery_web_frontend_entry_AdminMain__main__V();
-//# sourceMappingURL=admin.js.map
--- frontend/src/main/webapp/static/js/com.brewery.web.frontend.entry.-Conversations-Main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import * as $j_com$002ebrewery$002eweb$002efrontend$002eentry$002e$002dConversations$002dMain$0024 from "./com.brewery.web.frontend.entry.-Conversations-Main$.js";
-import * as $j_internal$002d07235935c771c9429268cb101d7f2e2e7f60988c from "./internal-07235935c771c9429268cb101d7f2e2e7f60988c.js";
-function $s_Lcom_brewery_web_frontend_entry_ConversationsMain__main__V() {
-  $j_com$002ebrewery$002eweb$002efrontend$002eentry$002e$002dConversations$002dMain$0024.$m_Lcom_brewery_web_frontend_entry_ConversationsMain$().main__V();
-}
-export { $s_Lcom_brewery_web_frontend_entry_ConversationsMain__main__V as $s_Lcom_brewery_web_frontend_entry_ConversationsMain__main__V };
-//# sourceMappingURL=com.brewery.web.frontend.entry.-Conversations-Main.js.map
